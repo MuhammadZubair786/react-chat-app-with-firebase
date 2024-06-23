f654d2bddd0d911ea3ad9bbc2bfa5e51a82f64a8
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2024-06-23 14:25:12 +0500

    Update firebase.jsx

--- src/Config/firebase.jsx
+++ src/Config/firebase.jsx
@@ -5,13 +5,7 @@ import "firebase/compat/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBkvRj66JKpqauZpIl5qHOmsDBt-WSjJxk",
-    authDomain: "booming-voice-411517.firebaseapp.com",
-    databaseURL: "https://booming-voice-411517-default-rtdb.firebaseio.com",
-    projectId: "booming-voice-411517",
-    storageBucket: "booming-voice-411517.appspot.com",
-    messagingSenderId: "11647279445",
-    appId: "1:11647279445:web:065a7f23f560b338f10fdc"
+  
   };
 
   firebase.initializeApp(firebaseConfig);
